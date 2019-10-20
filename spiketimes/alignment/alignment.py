b54018e6dda55ba5666fa6eccc00730ec50f9c7a
Ruairi-osul <rorylikesmusic@gmail.com>
2019-10-19 21:24:46 -0400

    modified and tested align_to

--- spiketimes/alignment/alignment.py
+++ spiketimes/alignment/alignment.py
@@ -1,9 +1,43 @@
 import numpy as np
 
 
-def align_to(to_be_aligned, to_align_to):
-    """Align to_be_aligned to to_align_to i.e. for each element in
-    to_be aligned, find the closest element in to_align_to with a lower value (occured before it)
+def align_to(to_be_aligned, to_align_to, no_beyond=False):
+    """Align to_be_aligned to to_align_to.
+    For each element in to_be aligned, find the closest element in to_align_to with a lower value.
+    Returns np.nan for elements in to_be_aligned smaller than the smallest element in to_be_aligned
+    Optionally specify no_beyond. If specified, sets all elements in to_be_aligned larger than to_align_to
+    equal to np.nan
     """
+
+    _to_be_aligned_isiter = False
+    _to_align_to_isiter = False
+    try:
+        [x for x in to_be_aligned]
+        _to_be_aligned_isiter = True
+    except TypeError:
+        pass
+    try:
+        [x for x in to_align_to]
+        _to_align_to_isiter = True
+    except TypeError:
+        pass
+    if not _to_align_to_isiter and _to_be_aligned_isiter:
+        raise TypeError(
+            "Must not pass two objects of length one."
+            "At least argument must be an iterable"
+        )
+    if not isinstance(to_be_aligned, np.ndarray) or not isinstance(
+        to_align_to, np.ndarray
+    ):
+        raise TypeError("Both arrays must be numpy arrays")
+
+    if not len(to_align_to.shape) == 1 and not len(to_be_aligned.shape) == 1:
+        raise ValueError("Must Pass in flat numpy arrays. Try your_array.flatten()")
+
+    if no_beyond:
+        to_be_aligned = to_be_aligned[to_be_aligned < np.max(to_align_to)]
+
     idx = np.searchsorted(to_align_to, to_be_aligned)
-    return to_be_aligned - to_align_to[idx - 1]
+    aligned_data = (to_be_aligned - to_align_to[idx - 1]).astype(np.float)
+    aligned_data[aligned_data < 0] = np.nan
+    return aligned_data
--- tests/test_alignment.py
+++ tests/test_alignment.py
@@ -1,19 +1,58 @@
 import pytest
 from spiketimes.alignment import align_to
+import numpy as np
 
 
 class TestAlignTo:
     def test_two_scaler_input(self):
         bad_input_1 = 1
         bad_input_2 = 2
-        with pytest.raises(ValueError):
+        with pytest.raises(TypeError):
             align_to(to_be_aligned=bad_input_1, to_align_to=bad_input_2)
 
     def test_list_input(self):
-        pass
+        bad_input_1 = [3, 2, 1, 3, 2]
+        bad_input_2 = [2, 3, 10, 2]
+        good_input = np.array(bad_input_1)
+
+        # one bad input
+        with pytest.raises(TypeError):
+            align_to(bad_input_1, good_input)
+            align_to(good_input, bad_input_2)
+
+        # one two bad inputs
+        with pytest.raises(TypeError):
+            align_to(bad_input_1, bad_input_2)
+
+    def test_1d_array(self):
+        # input arrays should be flat
+        bad_input = np.array([3, 2, 1, 3, 2]).reshape(5, 1)
+        good_input = np.array([32, 20, 10, 3, 2])
+
+        with pytest.raises(ValueError):
+            align_to(bad_input, good_input)
+            align_to(good_input, bad_input)
 
     def test_events_before_first_aligning_event(self):
-        pass
+        a = np.arange(0, 10, 1)
+        b = np.arange(3.5, 8.5, 1)
+        good_output = np.array(
+            [np.nan, np.nan, np.nan, np.nan, 0.5, 0.5, 0.5, 0.5, 0.5, 1.5]
+        )
+        actual_output = align_to(a, b)
+        np.testing.assert_allclose(actual_output, good_output)
 
-    def test_output_shape(self):
-        pass
+    def test_normal_case(self):
+        a = np.arange(0, 10, 2)
+        b = np.arange(0.6, 8, 2)
+        expected = np.array([np.nan, 1.4, 1.4, 1.4, 1.4])
+        actual = align_to(a, b)
+        np.testing.assert_allclose(actual, expected)
+
+    def test_no_beyond(self):
+        a = np.arange(0, 10, 2)
+        b = np.arange(1.1, 3, 0.5)
+        expected = np.array([np.nan, 0.4])
+        actual = align_to(a, b, no_beyond=True)
+        np.testing.assert_allclose(actual, expected)
+
