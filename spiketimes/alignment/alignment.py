91b5be51b2f1602ee92a95d740d201a452e72452
Ruairi-osul <rorylikesmusic@gmail.com>
2019-10-20 20:56:53 -0400

    tested negative align

--- spiketimes/alignment/alignment.py
+++ spiketimes/alignment/alignment.py
@@ -34,23 +34,52 @@ def align_to(to_be_aligned, to_align_to, no_beyond=False):
     if not len(to_align_to.shape) == 1 and not len(to_be_aligned.shape) == 1:
         raise ValueError("Must Pass in flat numpy arrays. Try your_array.flatten()")
 
-    if no_beyond:
-        to_be_aligned = to_be_aligned[to_be_aligned < np.max(to_align_to)]
-
     idx = np.searchsorted(to_align_to, to_be_aligned)
     aligned_data = (to_be_aligned - to_align_to[idx - 1]).astype(np.float)
     aligned_data[aligned_data < 0] = np.nan
+
+    if no_beyond:
+        aligned_data[to_be_aligned > np.max(to_align_to)] = np.nan
     return aligned_data
 
 
-def negative_align(to_be_aligned, to_align_to):
+def negative_align(to_be_aligned, to_align_to, no_before=False):
     """Align each element in to_be_aligned to the closest larger element
-    in to_align_to"""
+    in to_align_to
+    
+    Optionally return nan for elements in to_be_aligned occuring before the
+    first element in to_align_to"""
+
+    _to_be_aligned_isiter = False
+    _to_align_to_isiter = False
+    try:
+        [x for x in to_be_aligned]
+        _to_be_aligned_isiter = True
+    except TypeError:
+        pass
+    try:
+        [x for x in to_align_to]
+        _to_align_to_isiter = True
+    except TypeError:
+        pass
+    if not _to_align_to_isiter and _to_be_aligned_isiter:
+        raise TypeError(
+            "Must not pass two objects of length one."
+            "At least argument must be an iterable"
+        )
+
+    if not isinstance(to_be_aligned, np.ndarray) or not isinstance(
+        to_align_to, np.ndarray
+    ):
+        raise TypeError("Both arrays must be numpy arrays")
+
     max_idx = len(to_align_to) - 1
     idx = np.searchsorted(to_align_to, to_be_aligned).astype(np.int)
     idx[idx < max_idx] += 1
     aligned_data = (to_be_aligned - to_align_to[idx - 1]).astype(np.float)
     aligned_data[aligned_data > 0] = np.nan
+    if no_before:
+        aligned_data[to_be_aligned < np.min(to_align_to)] = np.nan
     return aligned_data
 
 
--- tests/test_alignment.py
+++ tests/test_alignment.py
@@ -1,5 +1,5 @@
 import pytest
-from spiketimes.alignment import align_to
+from spiketimes.alignment import align_to, negative_align
 import numpy as np
 
 
@@ -52,7 +52,58 @@ class TestAlignTo:
     def test_no_beyond(self):
         a = np.arange(0, 10, 2)
         b = np.arange(1.1, 3, 0.5)
-        expected = np.array([np.nan, 0.4])
+        expected = np.array([np.nan, 0.4, np.nan, np.nan, np.nan])
         actual = align_to(a, b, no_beyond=True)
         np.testing.assert_allclose(actual, expected)
 
+
+class TestNegativeAlign:
+    def test_two_scaler_input(self):
+        bad_input_1 = 1
+        bad_input_2 = 2
+        with pytest.raises(TypeError):
+            negative_align(to_be_aligned=bad_input_1, to_align_to=bad_input_2)
+
+    def test_list_input(self):
+        bad_input_1 = [3, 2, 1, 3, 2]
+        bad_input_2 = [2, 3, 10, 2]
+        good_input = np.array(bad_input_1)
+
+        # one bad input
+        with pytest.raises(TypeError):
+            negative_align(bad_input_1, good_input)
+            negative_align(good_input, bad_input_2)
+
+        # one two bad inputs
+        with pytest.raises(TypeError):
+            negative_align(bad_input_1, bad_input_2)
+
+    def test_1d_array(self):
+        # input arrays should be flat
+        bad_input = np.array([3, 2, 1, 3, 2]).reshape(5, 1)
+        good_input = np.array([32, 20, 10, 3, 2])
+
+        with pytest.raises(ValueError):
+            negative_align(bad_input, good_input)
+            negative_align(good_input, bad_input)
+
+    def test_events_after_first_aligning_event(self):
+        a = np.arange(7, 10, 1)
+        b = np.arange(3.5, 10.5, 1)
+        good_output = np.array([-0.5, -0.5, np.nan])
+        actual_output = negative_align(a, b)
+        np.testing.assert_allclose(actual_output, good_output)
+
+    def test_normal_case(self):
+        a = np.arange(0, 10, 2)
+        b = np.arange(0.6, 8, 2)
+        expected = np.array([-0.6, -0.6, -0.6, np.nan, np.nan])
+        actual = negative_align(a, b)
+        np.testing.assert_allclose(actual, expected)
+
+    def test_no_beyond(self):
+        a = np.arange(0, 10, 2)
+        b = np.arange(1.1, 3, 0.5)
+        expected = np.array([np.nan, -0.1, np.nan, np.nan, np.nan])
+        actual = negative_align(a, b, no_before=True)
+        np.testing.assert_allclose(actual, expected)
