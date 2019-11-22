112be4568089ec08e1c00817a66495f12a1b133a
Ruairi-osul <rorylikesmusic@gmail.com>
2019-11-22 10:59:37 -0500

    ifr finished

--- spiketimes/statistics/spiketrain_descriptive_statistics.py
+++ spiketimes/statistics/spiketrain_descriptive_statistics.py
@@ -4,10 +4,8 @@ import numpy as np
 def inter_spike_intervals(spiketrain):
     """given an array of spike times, returns an 
     array of interspike intervals
-    
     Params: 
         spiketrain: an array of spike times
-    
     reutrns:
         a numpy array of inter spike intervals"""
     # TODO tests
