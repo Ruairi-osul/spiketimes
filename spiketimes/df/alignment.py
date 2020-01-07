b1f500d3232d9f5d6c6a2c24d3c92d19c856d191
Ruairi <rorylikesmusic@gmail.com>
2020-01-07 16:36:58 -0500

    improved df alignment and binning documentation

--- spiketimes/df/alignment.py
+++ spiketimes/df/alignment.py
@@ -1,5 +1,29 @@
 import numpy as np
+import pandas as pd
 from ..alignment import align_to, negative_align
+
+
+def align_df(
+    df: pd.core.frame.DataFrame,
+    t_colname: str,
+    events: np.ndarray,
+    total_cycle: float,
+    t_before: float,
+):
+    """
+    Aligns a column in a df to events.
+    params:
+        df: the dataframe containing the data to be aligned
+        t_colname: the column name in df to be aligned
+        events: a series or numpy array of event timestamps to align to
+        total_cycle: the total time period of the event being aligned to
+        t_before: the desired negative window before the onset of the event to align to
+    returns:
+        a pd.Series of values in df[t_colname] aligned to events 
+    """
+    return df.assign(
+        pos=align_to(df[t_colname], events), neg=negative_align(df[t_colname], events),
+    ).apply(_compare_pos_neg, t_before=t_before, total_cycle=total_cycle, axis=1,)
 
 
 def _compare_pos_neg(row, t_before, total_cycle):
@@ -15,18 +39,3 @@ def _compare_pos_neg(row, t_before, total_cycle):
         else ret
     )
     return ret
-
-
-def align_df(df, t_colname, events, total_cycle, t_before):
-    """
-    Aligns a column in a df to events.
-    params:
-        df: the dataframe containing the data to be aligned
-        t_colname: the column name in df to be aligned
-        events: a series or numpy array of event timestamps to align to
-        total_cycle: the total time period of the event being aligned to
-        t_before: the desired negative window before the onset of the event to align to
-    """
-    return df.assign(
-        pos=align_to(df[t_colname], events), neg=negative_align(df[t_colname], events),
-    ).apply(_compare_pos_neg, t_before=t_before, total_cycle=total_cycle, axis=1,)
--- spiketimes/df/binning.py
+++ spiketimes/df/binning.py
@@ -1,12 +1,23 @@
 import numpy as np
+import pandas as pd
 
 
 def bin_df(
-    df, colname, bins, return_idx=False, return_values=True, bin_val_name="bin_val"
+    df: pd.core.frame.DataFrame,
+    colname: str,
+    bins: np.ndarray,
+    return_idx=False,
+    return_values=True,
+    bin_idx_name: str = "bin_idx",
+    bin_val_name: str = "bin_val",
 ):
-    df["bin_idx"] = np.digitize(df[colname], bins) - 1
+    """
+    Given a dataframe containing a continuous column, and bin values,
+    appends the appropriate index and or value to the dataframe
+    """
+    df[bin_idx_name] = np.digitize(df[colname], bins) - 1
     if return_values:
-        df[bin_val_name] = bins[df["bin_idx"].tolist()]
+        df[bin_val_name] = bins[df[bin_idx_name].tolist()]
     if not return_idx:
         df.drop("bin_idx", axis=1, inplace=True)
     return df
